46af3fcd2e0b42bb2d90553849e1544d212d87b8
Vibhuti Gohil <vibhutigohil77@gmail.com>
2023-04-20 13:13:21 -0400

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Headerbar from './components/Headerbar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Headerbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Headerbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Headerbar() {
+    return (
+        <Navbar className='shadow-sm p-3 mb-5 bg-white rounded'>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        src="../images/airbnb-logo.png"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Airbnb Logo"
+                    />
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+}
